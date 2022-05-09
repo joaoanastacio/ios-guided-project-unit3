e6ccd3f9b77b21c0dfa7b03969af8a76a81b53f6
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-05-09 11:47:03 -0700

    add: currently game state to git

---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Joao Victor Silva Anastacio on 2022-05-08.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    
+    var responses: [Answer]
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+    }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into: [AnimalType: Int]()) { (counts, answer) in
+            if let existingCount = counts[answer.type] {
+                counts[answer.type] = existingCount + 1
+            } else {
+                counts[answer.type] = 1
+            }
+        }
+        
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+        { (pair1, pair2) in
+            return pair1.value > pair2.value
+        })
+        
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+    
+    @IBAction func unwindToQuizIntroduction(segue:
+    UIStoryboardSegue) {
+        
+    }
+}
